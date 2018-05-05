47f638c74b40047b93ccfcf33fc34e5336e75219
eugeneloza <Loza@i.ua>
2018-05-05 12:36:53 +0300

    add license and unit comment

--- castlegis.pas
+++ castlegis.pas
@@ -1,3 +1,20 @@
+{
+  Copyright 2017-2018 Yevhen Loza, Michalis Kamburelis.
+
+  This file is part of "Castle Game Engine".
+
+  "Castle Game Engine" is free software; see the file COPYING.txt,
+  included in this distribution, for details about the copyright.
+
+  "Castle Game Engine" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  ----------------------------------------------------------------------------
+}
+
+{ Basic GIS operations on points and rendering of the base map }
+
 unit CastleGIS;
 
 {$mode objfpc}{$H+}
